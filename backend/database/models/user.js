5c1586cabc21d7afefc1264419634aad9aa03a9e
BrishaP <152220142+BrishaP@users.noreply.github.com>
2024-12-08 13:50:24 +0000

    adjusted model schema to remove name and instead use forename and surname

--- backend/database/models/user.js
+++ backend/database/models/user.js
@@ -1,10 +1,25 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
+  forename: {
+    type: String,
+    required: true,
+  },
+  surname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
 const User = mongoose.model('User', userSchema);
+
 export default User;
