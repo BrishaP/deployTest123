9b27fdf6959f30fa6adcad300cce69be5eb9d0f5
BrishaP <152220142+BrishaP@users.noreply.github.com>
2024-12-09 15:21:07 +0000

    added basic classnames to homepage and added some dummy text

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './HomePage.css'
+
+const HomePage = () => {
+  return (
+    <div className="home-container">
+      <div className="home-content">
+        <h1>Welcome: HomePage</h1>
+        <p>
+          Lorem ipsum odor amet, consectetuer adipiscing elit. Risus lacinia blandit neque tincidunt tempus sociosqu lectus. Nam donec conubia purus consequat feugiat primis. Ad blandit parturient quam netus iaculis? Magnis litora tortor conubia; vitae curabitur class. Vulputate posuere in lobortis odio dictum rhoncus rhoncus. Varius dignissim tortor fusce luctus proin. Vitae gravida parturient dictum pharetra justo sed dis. Dignissim integer consequat tempor aptent volutpat mauris nisi ultricies. Morbi semper vulputate sagittis libero sapien sapien, aliquam sodales?
+        </p>
+        <p>
+          Est nibh nullam lorem enim senectus natoque. Elit platea vehicula aliquet fusce nec quisque auctor auctor adipiscing. Potenti tempus libero quisque elementum erat hendrerit fringilla. Netus vel cras tempor dignissim per felis ullamcorper lacus. Montes sollicitudin adipiscing inceptos vehicula vehicula. Scelerisque integer molestie venenatis curabitur quis iaculis nec. Nunc magnis donec nascetur mus dolor dapibus praesent nullam. Dignissim congue arcu gravida habitasse hac torquent etiam. Placerat pretium sed integer, netus facilisi parturient.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
